37c8812f00b5a020c6a7478de89ce61ca1c3315e
dtabby <dtabby404@gmail.com>
2024-02-23 18:26:20 +0900

    Initial commit

---
+++ src/main/java/com/kkw/EventCafe/EventCafeApplication.java
@@ -0,0 +1,13 @@
+package com.kkw.EventCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkw/EventCafe/EventCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkw.EventCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
