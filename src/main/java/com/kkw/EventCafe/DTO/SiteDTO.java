2cd528da3af85c3240eded3a99b061253b73292b
dtabby <dtabby404@gmail.com>
2024-02-25 20:56:39 +0900

    SiteDTO 내용 수정

--- src/main/java/com/kkw/EventCafe/DTO/SiteDTO.java
+++ src/main/java/com/kkw/EventCafe/DTO/SiteDTO.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.ToString;
 
 @Getter // getter와 같은 역할을 한다.
-@ToString() // exclude에 해당하는 필드 값은 출력되지 않는다.
+@ToString()
 public class SiteDTO {
     private Long siteId;
     private String title;
